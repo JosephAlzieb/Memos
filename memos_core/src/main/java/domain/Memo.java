06c2e546872855b85043130f0f96c0244dae5d82
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-19 17:32:10 +0100

    attribute hinzugefügt. id, und person um eine Person zu referenzieren

--- memos_core/src/main/java/domain/Memo.java
+++ memos_core/src/main/java/domain/Memo.java
@@ -4,18 +4,22 @@ import java.time.LocalDate;
 
 public class Memo {
 
+    private int id;
     private  String titel;
     private  final LocalDate von;
     private  final LocalDate bis;
     private  String beschreibung;
     private  boolean erledigt;
+    private int person;
 
-    public Memo(String titel, LocalDate von, LocalDate bis, String beschreibung, boolean erledigt) {
+    public Memo(int id, String titel, LocalDate von, LocalDate bis, String beschreibung, boolean erledigt, int person) {
+        this.id = id;
         this.titel = titel;
         this.von = von;
         this.bis = bis;
         this.beschreibung = beschreibung;
         this.erledigt = erledigt;
+        this.person = person;
     }
 
     public String getTitel() {

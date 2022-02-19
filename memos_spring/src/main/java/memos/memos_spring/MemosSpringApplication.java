a75f9df4f98d63a548edcd81fe64452912da654f
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-02-19 17:02:37 +0100

    initial commit. ich werde nach  Zwiebeln-Architektur entwickeln. somit ich habe die 3 Schichten erstellt. spring ->app -> core <- app <- spring

---
+++ memos_spring/src/main/java/memos/memos_spring/MemosSpringApplication.java
@@ -0,0 +1,13 @@
+package memos.memos_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemosSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemosSpringApplication.class, args);
+    }
+
+}
---
+++ memos_spring/src/test/java/memos/memos_spring/MemosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package memos.memos_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemosSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
